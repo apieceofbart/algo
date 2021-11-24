78762409838df2ea7791783ffb71176ac0b1cda9
Bartek Kozera <bartek.kozera@gmail.com>
2021-11-24 21:47:49 +0100

    add in place quick sort

--- src/quickSort.ts
+++ src/quickSort.ts
@@ -17,3 +17,44 @@ export function quickSort(array: number[]): number[] {
 
   return [...quickSort(left), pivot, ...quickSort(right)];
 }
+
+function swap(array: unknown[], i: number, j: number) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+let c = 0;
+
+export function quickSortInPlace(
+  array: number[],
+  lowInit?: number,
+  highInit?: number
+) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const low = lowInit || 0;
+  const high = highInit ?? array.length - 1;
+  if (low >= high) {
+    return;
+  }
+
+  let i = low - 1;
+  let pivot = array[high];
+
+  for (let j = low; j < high; j++) {
+    if (array[j] < pivot) {
+      i++;
+      swap(array, i, j);
+    }
+  }
+
+  swap(array, i + 1, high);
+
+  quickSortInPlace(array, low, i);
+  quickSortInPlace(array, i + 2, high);
+
+  return array;
+}
--- test/quickSort.ts
+++ test/quickSort.ts
@@ -1,4 +1,4 @@
-import { quickSort } from "../src/quickSort";
+import { quickSort, quickSortInPlace } from "../src/quickSort";
 import * as chai from "chai";
 const { expect } = chai;
 
@@ -12,4 +12,19 @@ it("should return same array", () => {
 
 it("should sort array", () => {
   expect(quickSort([5, 4, 7, 2])).to.deep.equal([2, 4, 5, 7]);
+});
+
+it("should return empty array", () => {
+  expect(quickSortInPlace([])).to.deep.equal([]);
+});
+
+it("should return same array", () => {
+  expect(quickSortInPlace([1])).to.deep.equal([1]);
+});
+
+it("should sort array and return the same array", () => {
+  const arr = [5, 4, 7, 2];
+  const actual = quickSortInPlace(arr);
+  expect(actual).to.deep.equal([2, 4, 5, 7]);
+  expect(arr).to.equal(actual);
 });
