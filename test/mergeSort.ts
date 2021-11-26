9373ec36cce862053b7c225201255099f7d5030c
Bartek Kozera <bartek.kozera@gmail.com>
2021-11-26 21:07:38 +0100

    add describe blocks to test suites for nicer input

--- test/mergeSort.ts
+++ test/mergeSort.ts
@@ -1,15 +1,16 @@
 import { mergeSort } from "../src/mergeSort";
 import * as chai from "chai";
 const { expect } = chai;
+describe("merge sort", () => {
+  it("should return empty array", () => {
+    expect(mergeSort([])).to.deep.equal([]);
+  });
 
-it("should return empty array", () => {
-  expect(mergeSort([])).to.deep.equal([]);
-});
+  it("should return same array", () => {
+    expect(mergeSort([1])).to.deep.equal([1]);
+  });
 
-it("should return same array", () => {
-  expect(mergeSort([1])).to.deep.equal([1]);
-});
-
-it("should sort array", () => {
-  expect(mergeSort([5, 4, 7, 2])).to.deep.equal([2, 4, 5, 7]);
+  it("should sort array", () => {
+    expect(mergeSort([5, 4, 7, 2])).to.deep.equal([2, 4, 5, 7]);
+  });
 });
--- test/quickSort.ts
+++ test/quickSort.ts
@@ -2,29 +2,33 @@ import { quickSort, quickSortInPlace } from "../src/quickSort";
 import * as chai from "chai";
 const { expect } = chai;
 
-it("should return empty array", () => {
-  expect(quickSort([])).to.deep.equal([]);
-});
+describe("quick sort", () => {
+  it("should return empty array", () => {
+    expect(quickSort([])).to.deep.equal([]);
+  });
 
-it("should return same array", () => {
-  expect(quickSort([1])).to.deep.equal([1]);
-});
+  it("should return same array", () => {
+    expect(quickSort([1])).to.deep.equal([1]);
+  });
 
-it("should sort array", () => {
-  expect(quickSort([5, 4, 7, 2])).to.deep.equal([2, 4, 5, 7]);
-});
+  it("should sort array", () => {
+    expect(quickSort([5, 4, 7, 2])).to.deep.equal([2, 4, 5, 7]);
+  });
 
-it("should return empty array", () => {
-  expect(quickSortInPlace([])).to.deep.equal([]);
-});
+  describe("in place", () => {
+    it("should return empty array", () => {
+      expect(quickSortInPlace([])).to.deep.equal([]);
+    });
 
-it("should return same array", () => {
-  expect(quickSortInPlace([1])).to.deep.equal([1]);
-});
+    it("should return same array", () => {
+      expect(quickSortInPlace([1])).to.deep.equal([1]);
+    });
 
-it("should sort array and return the same array", () => {
-  const arr = [5, 4, 7, 2];
-  const actual = quickSortInPlace(arr);
-  expect(actual).to.deep.equal([2, 4, 5, 7]);
-  expect(arr).to.equal(actual);
+    it("should sort array and return the same array", () => {
+      const arr = [5, 4, 7, 2];
+      const actual = quickSortInPlace(arr);
+      expect(actual).to.deep.equal([2, 4, 5, 7]);
+      expect(arr).to.equal(actual);
+    });
+  });
 });
